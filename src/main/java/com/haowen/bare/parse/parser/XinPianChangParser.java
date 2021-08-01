e3493ab299d841423aab589bc7104b79fc449659
Li <haowenhello@gmail.com>
2021-08-01 15:30:10 +0800

    新片场

--- src/main/java/com/haowen/bare/parse/parser/XinPianChangParser.java
+++ src/main/java/com/haowen/bare/parse/parser/XinPianChangParser.java
@@ -1,5 +1,7 @@
 package com.haowen.bare.parse.parser;
 
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
 import cn.hutool.json.JSONUtil;
 import com.haowen.bare.parse.BareParser;
 import com.haowen.bare.parse.enums.MediaType;
@@ -16,6 +18,17 @@ import java.util.List;
 
 /**
  * 新片场解析
+ * ==============================================================
+ * User-Agent Mobile
+ * 1. 获取重定向地址
+ * 2. 获取分享链接ID（参数形式）
+ * 3. 请求接口获取数据json
+ * 4. 解析获取想要的结果
+ * --------------------------------------------------------------
+ * 标题 -> data -> title
+ * 封面 -> data -> cover
+ * 视频 -> data -> cover -> video -> content -> progressive(Array) => (url, profile, width, height)
+ * ==============================================================
  */
 @Component
 public class XinPianChangParser implements BareParser {
@@ -31,32 +44,41 @@ public class XinPianChangParser implements BareParser {
     @Override
     public BareResult parse(String url) throws IOException {
 
+        // 构建结果
+        BareResult result = new BareResult(MediaType.VIDEO);
+        List<BareResult.Video> videos = new ArrayList<>();
+        result.setVideos(videos);
+
         String userAgent = UserAgentUtil.getOne();
         // 获取分享资源信息
         String jsonStr = Jsoup
                 .connect(API + getItemId(UrlUtil.getRealUrl(userAgent, url)))
-                .header("User-Agent", userAgent)
+                .userAgent(userAgent)
                 .ignoreContentType(true)
                 .execute()
                 .body();
 
-        // 解析无水印资源
-        String videoUrl = (String) JSONUtil
-                .parseObj(jsonStr)
-                .getJSONObject("data")
-                .getJSONObject("video")
-                .getJSONObject("content")
-                .getJSONArray("progressive")
-                .getJSONObject(2)
-                .getObj("url");
+        JSONObject dataObject = JSONUtil.parseObj(jsonStr)
+                .getJSONObject("data");
 
-        // 构建结果
-        BareResult result = new BareResult();
-        result.setType(MediaType.VIDEO);
-        List<BareResult.Video> videos = new ArrayList<>();
-        result.setVideos(videos);
-        BareResult.Video videoResult = new BareResult.Video(videoUrl, null);
-        videos.add(videoResult);
+        // 标题、封面
+        result.setTitle(dataObject.getStr("title"))
+                .setCover(new BareResult.Image(dataObject.getStr("cover")));
+
+        JSONArray videoInfoList = dataObject.getJSONObject("video")
+                .getJSONObject("content")
+                .getJSONArray("progressive");
+
+        // 视频信息
+        for (int i = 0; i < videoInfoList.size(); i++) {
+            JSONObject item = videoInfoList.getJSONObject(i);
+            videos.add(new BareResult.Video(
+                    item.getStr("url"),
+                    item.getStr("profile"),
+                    item.getInt("width"),
+                    item.getInt("height")
+            ));
+        }
 
         return result;
     }
